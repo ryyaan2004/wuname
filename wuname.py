19e8820e5c2337211c3baab69177916f5bc072c4
ryyaan2004 <ryyaan2004@users.noreply.github.com>
2023-04-15 10:59:25 -0500

    Tolkien's Dwarves (#10)
    First new names are based on Tolkien's Dwarves

--- wuname.py
+++ wuname.py
@@ -4,9 +4,6 @@ import os
 
 app = Flask(__name__)
 
-LINE_ONE = '%s from this day forward'
-LINE_TWO = ' you will also be known as '
-
 
 @app.route('/')
 def index():
@@ -15,36 +12,58 @@ def index():
 
 @app.route('/wuami/<name>', methods=['GET'])
 def wu_am_i(name):
-    wuname = get_wu_name(name)
+    wuname = get_new_name(name, first_name_file="wu_adjs.txt", second_name_file="wu_nouns.txt")
     publish_event(wuname)
-    return common_return(wuname, '', '', wuname, name)
+    return common_return(message=wuname, new_name=wuname, original_name=name, request_type='wu')
 
 
 @app.route('/enterthewu/<name>', methods=['GET'])
 def enter_the_wu(name):
-    wuname = get_wu_name(name)
+    line_one = '{name} from this day forward'
+    line_two = ' you will also be known as {wuname}'
+    wuname = get_new_name(name, first_name_file="wu_adjs.txt", second_name_file="wu_nouns.txt")
     publish_event(wuname)
-    message = LINE_ONE % name + LINE_TWO + wuname
-    return common_return(message, LINE_ONE % name, LINE_TWO, wuname, name)
+    line_one = line_one.format(name=name)
+    line_two = line_two.format(wuname=wuname)
+    message = '''{line_one}\n{line_two}'''.format(line_one=line_one, line_two=line_two)
+    return common_return(message, wuname, name, request_type='wu')
 
 
-def common_return(message, line_one, line_two, wuname, original_name):
+@app.route('/durinsfolk/<name>', methods=['GET'])
+def durins_folk(name):
+    dwarf_name = get_new_name(name=name, first_name_file='dwarf_firstname.txt', second_name_file='dwarf_lastname.txt')
+    message = '''By the beard of Durin, {name}, you will henceforth be known as {dwarf_name}!
+May your days be filled with mining precious metals, regaling us with tales 
+of adventure, and upholding the honor of Durin's folk!'''.format(name=name, dwarf_name=dwarf_name)
+    publish_event(wuname=dwarf_name)
+    return common_return(message=message, new_name=dwarf_name, original_name=name, request_type="dwarf")
+
+
+@app.route('/dwarf/<name>', methods=['GET'])
+def dwarf(name):
+    dwarf_name = get_new_name(name=name, first_name_file='dwarf_firstname.txt', second_name_file='dwarf_lastname.txt')
+    publish_event(wuname=dwarf_name)
+    return common_return(message=dwarf_name, new_name=dwarf_name, original_name=name, request_type='dwarf')
+
+
+def common_return(message, new_name, original_name, request_type):
     if request_wants_type('application/json'):
         return jsonify({'message': message})
     elif request_wants_type('text/plain'):
         return message
     elif request_wants_type('application/xml'):
-        return render_template('response.xml', message=message)
+        return render_template('{}_response.xml'.format(request_type),
+                               message=message,
+                               )
     else:
-        return render_template('response.html',
-                               line_one=line_one,
-                               line_two=line_two,
-                               wuname=wuname,
+        return render_template('{}_response.html'.format(request_type),
+                               message=message,
+                               new_name=new_name,
                                original_name=original_name
                                )
 
 
-def get_wu_name(name):
+def get_new_name(name, first_name_file, second_name_file):
     import random
     seed = 1013
 
@@ -52,13 +71,13 @@ def get_wu_name(name):
         seed += ord(char) * (i + 1)
     random.seed(a=seed, version=2)
 
-    wu_adjs = read_file('assets/wu_adjs.txt')
+    first_names = read_file('assets/{}'.format(first_name_file))
 
-    wu_nouns = read_file('assets/wu_nouns.txt')
+    last_names = read_file('assets/{}'.format(second_name_file))
 
-    return "{adj} {noun}".format(
-        adj=wu_adjs[random.randint(0, len(wu_adjs) - 1)],
-        noun=wu_nouns[random.randint(0, len(wu_nouns) - 1)]
+    return "{first} {last}".format(
+        first=first_names[random.randint(0, len(first_names) - 1)],
+        last=last_names[random.randint(0, len(last_names) - 1)]
     )
 
 
@@ -79,8 +98,9 @@ def request_wants_type(type):
 
 
 def publish_event(wuname):
-    from google.cloud import pubsub_v1
+    # any value for PUBLISH_METRICS will result in True here
     if os.getenv('PUBLISH_METRICS', False):
+        from google.cloud import pubsub_v1
         publisher = pubsub_v1.PublisherClient()
         topic_name = 'projects/{project_id}/topics/{topic}'.format(
             project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
